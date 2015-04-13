2d40cc01eeead48506452d24ed7061bb4e8b2210
Marco Pivetta <ocramius@gmail.com>
2015-04-13 07:09:33 +0100

    Separating examples

--- run.sh
+++ run.sh
@@ -3,7 +3,14 @@
 curl -sS https://getcomposer.org/installer | php
 ./composer.phar install
 
+echo "Populating Database"
 time php populate-db.php
+
+echo "Lazy loading example"
 time php lazy-loading.php
+
+echo "Single fetch-join example"
 time php single-fetch-join.php
+
+echo "Multi-step hydration example"
 time php multi-step-hydration.php
