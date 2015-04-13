9ed157607a301f42cd01d797e14cd52f4ac6b380
Marco Pivetta <ocramius@gmail.com>
2015-04-13 07:10:35 +0100

    Separators in output

--- run.sh
+++ run.sh
@@ -3,14 +3,25 @@
 curl -sS https://getcomposer.org/installer | php
 ./composer.phar install
 
+SEPARATOR="###############################"
+
+echo "$SEPARATOR"
 echo "Populating Database"
+echo "$SEPARATOR"
 time php populate-db.php
 
+echo "$SEPARATOR"
 echo "Lazy loading example"
+echo "$SEPARATOR"
 time php lazy-loading.php
 
+echo "$SEPARATOR"
 echo "Single fetch-join example"
+echo "$SEPARATOR"
 time php single-fetch-join.php
 
+echo "$SEPARATOR"
 echo "Multi-step hydration example"
+echo "$SEPARATOR"
+
 time php multi-step-hydration.php
